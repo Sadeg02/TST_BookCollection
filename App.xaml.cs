a31c7ffae033ed80373b9417b624e04adc71a48e
Sadeg02 <mich2002m@gmail.com>
2025-05-27 12:18:47 +0200

    initialize wpf project

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TSD_BookCollection;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
