08388d98760f62e7b9de9268368d82d94b7a57b2
Sadeg02 <mich2002m@gmail.com>
2025-05-27 13:36:44 +0200

    Add detail panel for selected book

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,40 @@
-﻿using System.Windows;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace TSD_BookCollection
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private Book selectedBook;
+
         public List<Book> Books { get; set; }
+
+        public Book SelectedBook
+        {
+            get => selectedBook;
+            set
+            {
+                    selectedBook = value;
+                     PropertyChanged?.Invoke(this,
+               new PropertyChangedEventArgs(nameof(selectedBook)));
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
             Books = MyBookCollection.GetMyCollection();
+            SelectedBook = Books[0]; // Set default selected book
             DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
